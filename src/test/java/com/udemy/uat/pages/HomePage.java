35a2bca444cd67394ae929b71248a50e7a36fd9c
Elif-Pehlivan <1325elifpehlivan@gmail.com>
2017-09-27 16:15:06 -0400

    Second change

--- src/test/java/com/udemy/uat/pages/HomePage.java
+++ src/test/java/com/udemy/uat/pages/HomePage.java
@@ -7,39 +7,40 @@ import org.openqa.selenium.support.PageFactory;
 import com.udemy.uat.utilities.Driver;
 
 public class HomePage {
-	public HomePage(){
+	public HomePage() {
 		PageFactory.initElements(Driver.getInstance(), this);
 	}
-	//THis is the first change
-	@FindBy(id="id_email")
+
+	// THis is the first change
+	// THis is the second change
+	@FindBy(id = "id_email")
 	public WebElement emailId;
-	
-	@FindBy(id="id_password")
+
+	@FindBy(id = "id_password")
 	public WebElement password;
-	
-	@FindBy(id="submit-id-submit")
+
+	@FindBy(id = "submit-id-submit")
 	public WebElement submitButton;
-	
-	@FindBy(xpath="(//div[@ng-click='attemptAction($event)'])[2]")
+
+	@FindBy(xpath = "(//div[@ng-click='attemptAction($event)'])[2]")
 	public WebElement signUpButton;
-	
-	@FindBy(id="q")
+
+	@FindBy(id = "q")
 	public WebElement searchBox;
-	
-	@FindBy(xpath="//button[@type='submit']")
+
+	@FindBy(xpath = "//button[@type='submit']")
 	public WebElement searchButton;
-	
-	@FindBy(xpath="(//div[@ng-click='attemptAction($event)'])[1]")
+
+	@FindBy(xpath = "(//div[@ng-click='attemptAction($event)'])[1]")
 	public WebElement logInButton;
-	
-	@FindBy(id="id_fullname")
+
+	@FindBy(id = "id_fullname")
 	public WebElement fullName;
-	
-	@FindBy(xpath="//a[@href='/user/logout/?h=E0AdcVhQRXsBUQ%3D%3D']")
+
+	@FindBy(xpath = "//a[@href='/user/logout/?h=E0AdcVhQRXsBUQ%3D%3D']")
 	public WebElement logOut;
-	
-	@FindBy(xpath="(//span[@class='user__no-avatar'])[1]")
+
+	@FindBy(xpath = "(//span[@class='user__no-avatar'])[1]")
 	public WebElement nameAvatar;
-	
 
 }
